39b69ab75f9f222380e50dcbc4d45211a6fb397e
Oskar Carl <oskar.carl@uni-due.de>
2019-02-06 13:49:20 +0100

    FunctionType.spec at ~50% coverage

--- compiler/types/tests/FunctionType.spec.ts
+++ compiler/types/tests/FunctionType.spec.ts
@@ -1,7 +1,8 @@
 import { expect } from 'chai'
 import * as m from 'ts-mockito'
 
-import { FunctionType, Type, BasicType } from '../'
+import { FunctionType, BasicType } from '../'
+import { FunctionParameter } from '../../scopes';
 import { Group } from '../../group';
 
 describe('empty FunctionType', () => {
@@ -36,7 +37,7 @@ describe('empty FunctionType', () => {
     })
 
     it('createGroups() returns an empty group map', () => {
-        expect(instance.createGroups()).to.deep.equal(new Map<string, Group>())
+        expect(instance.createGroups()).to.be.of.length(0)
     })
 })
 
@@ -48,7 +49,6 @@ describe('FunctionType with BasicType output', () => {
         // let returnType: Type = m.mock(BasicType)
         // m.when(returnType.toString()).thenReturn('void')
         instance = new FunctionType()
-
         // instance.returnType = m.instance(returnType)
         instance.returnType = new BasicType('void')
     })
@@ -77,7 +77,63 @@ describe('FunctionType with BasicType output', () => {
         expect(instance.isAsync()).to.be.false
     })
 
-    it('createGroups() returns an empty group map', () => {
-        expect(instance.createGroups()).to.nested.include({"return": Group})
+    it('createGroups() returns a group map containing only the return group', () => {
+        let result: Map<string, Group> = instance.createGroups()
+        expect(result).to.be.of.length(1)
+        expect(result).to.have.all.keys('return')
+    })
+})
+
+describe('FunctionType with two params and BasicType output', () => {
+    let instance: FunctionType
+    let param1: FunctionParameter
+    let param2: FunctionParameter
+
+    before(() => {
+        param1 = m.mock(FunctionParameter)
+        m.when(param1.name).thenReturn('param1')
+        m.when(param1.ellipsis).thenReturn(false)
+        m.when(param1.type).thenReturn(new BasicType('int'))
+        param1 = m.instance(param1)
+
+        param2 = m.mock(FunctionParameter)
+        m.when(param2.name).thenReturn('param2')
+        m.when(param2.ellipsis).thenReturn(true)
+        m.when(param2.type).thenReturn(new BasicType('float'))
+        param2 = m.instance(param2)
+
+        instance = new FunctionType()
+        instance.returnType = new BasicType('void')
+        instance.parameters = [param1, param2]
+    })
+
+    it('toString() works', () => {
+        expect(instance.toString()).to.equal("(int,...float) => void")
+    })
+
+    it('toTypeCodeString() works', () => {
+        expect(instance.toTypeCodeString()).to.equal("(int,...float) => void")
+    })
+
+    it('hasEllipsis() returns true', () => {
+        expect(instance.hasEllipsis()).to.be.true
+    })
+
+    it('lastParameter() returns undefined', () => {
+        expect(instance.lastParameter()).to.equal(param2)
+    })
+
+    it('requiredParamterCount() returns 1, ignoring the ellipsis', () => {
+        expect(instance.requiredParameterCount()).to.equal(1)
+    })
+
+    it('isAsync() returns false', () => {
+        expect(instance.isAsync()).to.be.false
+    })
+
+    it('createGroups() returns a group map with both params and the return', () => {
+        let result: Map<string, Group> = instance.createGroups()
+        expect(result).to.be.of.length(3)
+        expect(result).to.have.all.keys('param1', 'param2', 'return')
     })
 })
