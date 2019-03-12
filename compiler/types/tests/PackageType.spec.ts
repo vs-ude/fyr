51dc881a41d17e6ad6baa0928d20e1bef9448ba0
Oskar Carl <oskar.carl@uni-due.de>
2019-03-12 18:01:26 +0100

    another test

---
+++ compiler/types/tests/PackageType.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import * as m from 'ts-mockito'
+
+import { PackageType } from '../'
+import { Package } from '../../pkg'
+
+describe('empty PackageType', () => {
+    let instance: PackageType
+
+    before(() => {
+        instance = new PackageType(null, null, null)
+    })
+
+    it('toString()', () => {
+        expect(instance.toString()).to.equal('package null')
+    })
+
+    it('toTypeCodeString()', () => {
+        expect(() => instance.toTypeCodeString()).to.throw('Implementation error')
+    })
+})
+
+describe('filled PackageType', () => {
+    let instance: PackageType
+
+    before(() => {
+        let pkg: Package = m.mock(Package)
+        instance = new PackageType('test', pkg, null)
+    })
+
+    it('toString()', () => {
+        expect(instance.toString()).to.equal('package test')
+    })
+
+    it('toTypeCodeString()', () => {
+        expect(() => instance.toTypeCodeString()).to.throw('Implementation error')
+    })
+})
