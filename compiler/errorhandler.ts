f8a720a1411ac7087008f10ae4cc63a517009a73
Oskar Carl <oskar.carl@uni-due.de>
2019-03-12 17:58:39 +0100

    emulate node error printing; fixes #14

--- compiler/errorhandler.ts
+++ compiler/errorhandler.ts
@@ -5,6 +5,8 @@ import {SyntaxError} from "./parser";
 import {TypeError} from "./typecheck";
 import {ImportError} from "./pkg";
 
+import { readFileSync } from 'fs'
+
 export interface ErrorHandler {
     handle(e: Error)
 }
@@ -25,8 +27,42 @@ export class StdErrorOutput implements ErrorHandler {
             }
             return
         } else {
-            console.log(e);
-            throw e;
+            console.error(this.buildOffendingLineString(e.stack))
+            console.error(e)
+            process.exit(1)
         }
+    }
+
+    /**
+     * This generates a string of the offending line in the file in the style of NodeJS.
+     * The offending file and line are extracted from the given stack trace.
+     * There is definitely room for optimization as it currently only loops through all lines in the file.
+     *
+     * @param stack The stack trace string.
+     */
+    private buildOffendingLineString(stack: string) {
+        let fileAndLocation: Array<string> = stack
+            .split('\n')[1]
+            .replace(/\s*at\s*[\w\.]*\s*\(/i, '') // remove the cruft before the path
+            .replace(')', '')
+            .split(':')
+
+        let file: string = fileAndLocation[0]
+        let lineNumber: number = Number(fileAndLocation[1])
+        let column: number = Number(fileAndLocation[2])
+
+        let outputLine: string = file + ':' + lineNumber + '\n'
+        let currentLineNumber: number = 1
+        readFileSync(file).toString().split('\n').forEach((line) => {
+            if (currentLineNumber == lineNumber) {
+                outputLine = outputLine + line + '\n'
+            }
+            currentLineNumber++
+        })
+
+        for (let i: number = 1; i < column; i++) {
+            outputLine = outputLine + ' '
+        }
+        return outputLine + '^\n'
     }
 }
